ba5b286bbd9cb9e1f7dd78ab160856a5868df4bf
Danielpqueiroz <112348645+Danielpqueiroz@users.noreply.github.com>
2024-03-11 17:27:38 -0300

    Lista 01

---
+++ Lista01Java/src/main/java/org/example/Ex01NumeroPrimo.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Ex01NumeroPrimo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um número inteiro: ");
+        int number = sc.nextInt();
+        int cont = 0;
+        if (number > 1){
+
+            for (int i = 2; i <= number-1; i++){
+                if (number % i == 0){
+                    cont += 1;
+                }
+            }
+
+        }else if (cont == 0){
+            System.out.println("O número informado é primo!");
+        } else{
+            System.out.println("O número informado não é primo!");
+        }
+
+    }
+
+
+}
